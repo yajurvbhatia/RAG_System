4ac2b7a412bff058e2b98a460b11685bc5df4177
Yajurv Bhatia <yajurvbhatia@Yajurvs-MacBook-Pro.local>
2025-04-08 13:53:50 +0530

    structured tool IOs + new web_search tool

--- agent_tools.py
+++ agent_tools.py
@@ -1,23 +1,32 @@
 from langchain.tools import Tool
 from fastapi import HTTPException
 import os
+# from langchain.tools import tool
+from langchain.tools import StructuredTool
+from pydantic import BaseModel, Field
+from typing import Annotated
+from typing import List
 
 from utils import connect_to_db, embedder, active_sessions
-from config import OPENAI_API_KEY
-
+from config import OPENAI_API_KEY, SERP_API_KEY
+import requests
 
 if not os.environ.get("OPENAI_API_KEY"):
     os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-# Define Retrieval Tool
-def retrieve(query):
+class RetrievalInput(BaseModel):
+    query: str = Field(description="The search query")
+
+class RetrievalOutput(BaseModel):
+    success: bool = Field(description="True if relevant documents are found, False if no relevant documents are found.")
+    info: str = Field(description="Aggregated information from all the relevant documents found.")
+
+def retrieval_tool_fn(query: str):
     top_k = 5
     try:
-        # Getting query vector
         query_vector = embedder.embed_query(query)
         vector_str = "[" + ",".join([str(x) for x in query_vector]) + "]"
 
-        # Fetching similar vectors
         conn = connect_to_db()
         cursor = conn.cursor()
 
@@ -26,19 +35,35 @@ def retrieve(query):
             (vector_str, top_k)
         )
         similar_vectors = cursor.fetchall()
-        
-        relevant_info = [item[0] for item in similar_vectors]
-        return f"relevant information: {relevant_info}"
-        
+
+        relevant_info = ""
+        for item in similar_vectors:
+            if item[1] < 0.20:
+                relevant_info += str(item[0])
+
+        if not relevant_info:
+            return RetrievalOutput(success=False, info="")
+
+        return RetrievalOutput(success=True, info=relevant_info)
+
     except Exception as e:
         print(f"Error in Retrieval tool: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
-retrieval_tool = Tool(
-            name="Retrieval Tool",
-            func=retrieve,
-            description="Use this tool to retrieve relevant knowledge."
-        )
+retrieval_tool = StructuredTool.from_function(
+    name="retrieval_tool",
+    description="Searches through documents for a query string.",
+    func=retrieval_tool_fn,
+    args_schema=RetrievalInput
+)
+
+
+class SaveConversationInput(BaseModel):
+    session_id: str = Field(description="The id of the ongoing session.")
+
+class SaveConversationOutput(BaseModel):
+    success: bool = Field(description="True if conversation was saved properly, False if there was an error in saving the conversation.")
+    error: str = Field(description="Detail of the error that occured while saving the conversation, Empty if no error occured.")
 
 # Function to save conversation to PostgreSQL
 def db_save_conversation(session_id: str):
@@ -94,22 +119,34 @@ def db_save_conversation(session_id: str):
         cursor.close()
         conn.close()
 
-        return "Final Answer: Database updated successfully!"
+        return SaveConversationOutput(success=True, error="")
 
     except Exception as e:
         print(f"Error in db_update_tool: {e}")
-        return f"DB update failed: {e}"
+        return SaveConversationOutput(success=False, error=e)
 
-db_save_conversation_tool = Tool(
+db_save_conversation_tool = StructuredTool.from_function(
     name="DB Update Tool",
     func=db_save_conversation,
-    description=(
-        """Use this tool ONLY if the user's query clearly requests saving or updating the conversation history.
+    description="""Use this tool ONLY if the user's query clearly requests saving or updating the conversation history.
         This tool appends a new message (latest user query and agent response) to the chat history 
         in the PostgreSQL database. Use it when the user says something like 'save this', 'log this', or 'remember this'.
-        Pass the 36 character length session_id as an input to this tool and nothing else."""
-    )
+        Pass the 36 character length session_id as an input to this tool and nothing else.""",
+    args_schema=SaveConversationInput
 )
+
+class GetConversationsInput(BaseModel):
+    user_id: str = Field(description="The id of the user whose conversations need to be fetched.")
+
+class Conversation(BaseModel):
+    conversation_id: int = Field(description="The id of the conversation the agent has had with the user.")
+    chat_history: str = Field(description="The history of the conversation as a string.")
+
+class GetConversationsOutput(BaseModel):
+    success: bool = Field(description="True if prior conversations for the user are found, False if none are found.")
+    user_id: str = Field(description="The id of the user whose conversations need to be fetched.")
+    conversations: List[Conversation]
+    error: str = Field(description="Detail of the error that occured while getting the conversations, Empty if no error occured.")
 
 # Function to get all conversations for a given user_id
 def db_get_user_conversations(user_id: int):
@@ -132,26 +169,61 @@ def db_get_user_conversations(user_id: int):
         cursor.close()
         conn.close()
 
-        # Format the results as a list of dictionaries
-        result = [
-            {"conversation_id": row[0], "chat_history": row[1]}
-            for row in conversations
-        ]
-        return "Final Answer: " + str(result)
+        conversations = [Conversation(conversation_id=row[0], chat_history=row[1]) for row in conversations]
+        
+        return GetConversationsOutput(success=True, user_id=user_id, conversations=conversations, error="")
 
     except Exception as e:
         print(f"Error in db_read_tool: {e}")
-        raise HTTPException(status_code=500, detail=f"DB read failed: {e}")
+        return GetConversationsOutput(success=False, user_id=user_id, conversations=[], error=e)
 
-db_get_user_conversations_tool = Tool(
+
+db_get_user_conversations_tool = StructuredTool.from_function(
     name="DB Read Tool",
     func=db_get_user_conversations,
-    description=(
-        """Connects to the PostgreSQL server and retrieves all conversations for a given user ID.
+    description="""Connects to the PostgreSQL server and retrieves all conversations for a given user ID.
         Args:
         user_id (int): The ID of the user whose conversations are to be retrieved. Do not pass anything else into the function.
         Returns:
         list: A list of conversations for the given user ID.
-        """
-    )
+        """,
+    args_schema=GetConversationsInput
+)
+
+class WebSearchInput(BaseModel):
+    query: str = Field(description="The search query used to fetch information from the internet.")
+
+class WebSearchOutput(BaseModel):
+    success: bool = Field(description="True if the web search is successful, False if it is not.")
+    result: str = Field(description="Aggregated information from the internet relevant to the search query.")
+    error: str = Field(description="Details of an error that occured, Empty if no error occurs.")
+
+# Function to search the web for information
+def web_search(query: str):
+    try:
+        url = "https://serpapi.com/search"
+        params = {
+            "engine": "google",
+            "q": query,
+            "api_key": SERP_API_KEY,
+            "start": 0,
+            "num": 5
+        }
+
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        response = response.json()
+
+        organic_results = response["organic_results"]
+        all_snippets = '.'.join([str(result["snippet"]) for result in organic_results])
+        return WebSearchOutput(success=True, result=all_snippets, error="")
+    
+    except Exception as e:
+        return WebSearchOutput(success=False, result="", error=e)
+
+get_info_from_web = Tool(
+    name="Web Search Tool",
+    func=web_search,
+    description="""Query the internet to get multiple short descriptions with information from different sources on the web.""",
+    args_schema=WebSearchInput
 )
--- agents.py
+++ agents.py
@@ -3,7 +3,7 @@ import os
 
 from config import OPENAI_API_KEY
 from utils import active_sessions
-from agent_tools import retrieval_tool, db_save_conversation_tool, db_get_user_conversations_tool
+from agent_tools import retrieval_tool, db_save_conversation_tool, db_get_user_conversations_tool, get_info_from_web
 
 
 if not os.environ.get("OPENAI_API_KEY"):
@@ -12,18 +12,22 @@ if not os.environ.get("OPENAI_API_KEY"):
 OPTIMIZED_AGENT_PROMPT = """
 Assistant Role:
 You are a helpful assistant and you have three main tasks to fulfill:
-1. Answer any knowledge based user questions by summarizing information that you get from the retrieval_tool.
+1. Try to answer any knowledge based user questions by summarizing information that you get from the retrieval_tool.
+Determine if the retrieved information is relevant to the query or not. If it is not, then use web search instead.
 2. Save conversations in the database when prompted by the user, by using the db_save_conversation_tool.
 3. Fetch all conversation history for a given user by using db_get_user_conversations_tool.
-4. Answer any general inquiries by not using any tool whatsoever, and only using the prior conversation instead.
+4. Answer any knowledge based questions using the web_search tool, if the retrieval tool returns irrelevant information.
+5. Answer any general inquiries by not using any tool whatsoever, and only using the prior conversation instead.
 
 Tool Instructions:
-- retrieval_tool: Clarify the query, retrieve factual info, and summarize it.
-If no relevant information is found, reply "I'm unable to answer that due to insufficient information."
+- retrieval_tool: Clarify the query, retrieve factual info, and summarize it. Only pass the query to this tool, nothing else.
+If no relevant information is found, use the web search tool to get information from the internet.
+If the retrieval_tool output contains "RETRIEVAL_FAILED", then call get_info_from_web.
 - db_save_conversation_tool: Invoke only if the user says something along the lines of "save this",
 "log this", or "remember this" (case-insensitive). Use only the provided 36-character session_id - session_id as the input to the tool.
 - db_get_user_conversations_tool: When requested, use the user_id to get all conversations with the user.
 If none exist, respond "No conversations found for the provided session ID." Always include the conversation_id.
+- get_info_from_web: Use this tool, when the retrieve tool, does not have the relevant information that would answer the user query.
 
 Rules:
 - All responses must be easy to understand and in a single string.
@@ -37,13 +41,16 @@ def create_agent(llm, session_id):
 
     # Initialize LangChain Agent
     agent = initialize_agent(
-        tools=[retrieval_tool, db_save_conversation_tool, db_get_user_conversations_tool],
+        tools=[retrieval_tool, db_save_conversation_tool, db_get_user_conversations_tool, get_info_from_web],
         llm=llm,
         memory=active_sessions[session_id].memory,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        # agent = AgentType.OPENAI_FUNCTIONS,
+        # agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        agent_type="structured-chat-zero-shot-react-description",
+        # agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         agent_kwargs={"prefix": OPTIMIZED_AGENT_PROMPT},
         verbose=True,
-        max_iterations=2,
+        max_iterations=3,
         early_stopping_method="generate",
         handle_parsing_errors=True
     )
